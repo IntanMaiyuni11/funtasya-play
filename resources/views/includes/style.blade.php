9c7342c037161c6e59a324b1dc2f0b60e52213a9
intan maiyuni <143939927+IntanMaiyuni@users.noreply.github.com>
2026-04-23 14:31:16 +0700

    Fix: comment out vite and use cdn

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,4 +1,4 @@
-@vite(['resources/css/app.css','resources/js/app.js'])
+{{-- @vite(['resources/css/app.css','resources/js/app.js']) --}}
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
