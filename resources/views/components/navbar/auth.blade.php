923d8aeceddf1db3e6b48e82384ada75cfbe69ed
IntanMaiyuni11 <intanmaiyuni1717@gmail.com>
2026-05-30 18:38:46 +0700

    Update auth.blade.php

--- resources/views/components/navbar/auth.blade.php
+++ resources/views/components/navbar/auth.blade.php
@@ -1,11 +1,21 @@
 <nav class="bg-white border-b">
+
     <div class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+
         <a href="{{ route('home') }}">
-            <img src="/images/logo_funtasyaplay.png" alt="Logo" class="h-8 md:h-10" />
+            <img src="{{ asset('images3/logo.png') }}"
+                 alt="PlayLearn"
+                 class="h-12">
         </a>
 
-        <a href="" target="_blank" class="text-xs md:text-sm font-bold text-[#335fff] flex items-center gap-2">
-            <span class="hidden sm:inline">Butuh bantuan?</span>
-        </a>
+        <div class="hidden md:flex items-center gap-2 text-sm font-medium text-purple-600">
+
+            <i class="fa-solid fa-headset"></i>
+
+            <span>Butuh bantuan?</span>
+
+        </div>
+
     </div>
+
 </nav>
