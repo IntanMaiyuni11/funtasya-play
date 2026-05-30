601d088e3304f2daef1c375f8091e3cb33a59ad5
IntanMaiyuni11 <intanmaiyuni1717@gmail.com>
2026-05-30 18:40:40 +0700

    Update style.blade.php

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,7 +1,8 @@
-{{-- @vite(['resources/css/app.css','resources/js/app.js']) --}}
+@vite(['resources/css/app.css','resources/js/app.js'])
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-<script src="https://cdn.tailwindcss.com"></script>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
